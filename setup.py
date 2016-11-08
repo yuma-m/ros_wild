b96f6b74ca6c69a99cbaf2e789932f5e0b2d3559
Yuma.M <yuma-m@users.noreply.github.com>
2016-11-08 22:02:50 +0900

    Create empty package

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+setup_args = generate_distutils_setup(
+    package_dir={'': 'src'}
+)
+
+setup(**setup_args)
---
+++ test/example-test.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from nose.tools import assert_equal
+
+def test_example():
+    assert_equal(2, 1 + 1)
