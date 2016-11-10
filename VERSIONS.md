88a3bdbb49d3c94c89e8be65d6a39dab83845c5c
Yuma.M <yuma-m@users.noreply.github.com>
2016-11-10 20:21:30 +0900

    Add a node to echo multiple topics

--- VERSIONS.md
+++ VERSIONS.md
@@ -1,3 +1,7 @@
+## 0.3.0
+
+- Create a node to echo topics
+
 ## 0.2.0
 
 - Create wildcard publisher
--- src/ros_wild/subscriber.py
+++ src/ros_wild/subscriber.py
@@ -21,3 +21,17 @@ class Subscriber(Topic):
     @property
     def subscribed_topics(self):
         return self._topics.keys()
+
+
+class EchoSubscriber(Subscriber):
+    u""" Wildcard subscriber to echo topics """
+    def __init__(self, name_regex, data_class, *args, **kwargs):
+        u""" Constructor of wildcard subscriber
+
+        :param str name_regex: Regular expression of topic name
+        """
+        super(EchoSubscriber, self).__init__(name_regex, data_class, *args, **kwargs)
+
+    def _call_pub_or_sub(self, topic):
+        pub_or_sub = self._pub_or_sub(topic, self._data_class, *self._args, callback_args=topic, **self._kwargs)
+        self._topics[topic] = pub_or_sub
